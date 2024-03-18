6a956ddd32c40e032f12867dfab599e5694b2ae6
YashBhansari <140247209+YashBhansari@users.noreply.github.com>
2024-03-18 19:14:36 +0530

    Update script.js added backSpace()

--- script.js
+++ script.js
@@ -1,71 +1,31 @@
-body {
-    background-image: url(images/bg.jpg);
-    height: 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    /* margin: 0; */
-    overflow: hidden;
+let display = document.getElementById("display");
+
+function clearDisplay(){
+    display.value = ""; 
 }
 
-#position {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    margin: 0;
+function backSpace() {
+    display.value = display.value.slice(0, -1);
 }
 
-#calci {
-
-    margin-left: 5px;
-    white-space: nowrap;
-    background-color: rgb(86, 86, 86);
-    padding: 5px 10px 10px 5px;
-    border: solid 3px;
-    border-radius: 20px;
+function appendToDisplay(input){
+    display.value += input; 
 }
 
-input {
-    height: 40px;
-    width: 170px;
-    vertical-align: middle;
-    text-align: right;
-    font-size: large;
-    margin-top: 5px;
-    margin-left: 5px;
-    border: black solid 2px;
-    border-radius: 10px;
-    padding-right: 10px;
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }catch(error){
+        display.value = "Error";
+    }
 }
 
-button {
-    width: 40px;
-    height: 40px;
-    border-radius: 17px;
-    margin-top: 5px;
-    margin-left: 5px;
-    background-color: rgb(182, 182, 182);
+function square(){
+    calculate();
+    display.value = Math.pow(display.value,2);
 }
 
-button:not(.num) {
-    background-color: rgb(255, 160, 58);
-}
-
-button:hover {
-    cursor: pointer;
-    opacity: 0.6;
-}
-
-button:active {
-    opacity: 0.3;
-}
-
-#credit {
-    position: relative;
-    bottom: 70px;
-    float: inline-end;
-    text-align: right;
-    color: azure;
-    font-family: cursive;
+function root(){
+    calculate();
+    display.value = Math.sqrt(display.value);
 }
